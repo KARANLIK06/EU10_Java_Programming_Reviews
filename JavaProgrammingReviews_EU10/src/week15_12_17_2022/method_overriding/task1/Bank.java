c016322b72f460386f0e1f1b96287633c5c1cdce
Ali KARANLIK <alikaranlik3112@gmail.com>
2022-12-18 12:17:45 +0300

    changed

--- JavaProgrammingReviews_EU10/src/week15_12_17_2022/method_overriding/task1/Bank.java
+++ JavaProgrammingReviews_EU10/src/week15_12_17_2022/method_overriding/task1/Bank.java
@@ -2,16 +2,16 @@ package week15_12_17_2022.method_overriding.task1;
 
 public class Bank {
 
-    private int capital=10000;
+    private int capital = 10000;
 
-    public int getCapital(){
+    public int getCapital() {
         return capital;
     }
 
     // How we can get the class name?
     //The answer is getClass().getSimpleName()
 
-    public void calculateTheInterest(){
+    public void calculateTheInterest() {
         System.out.println("Interest of the " + getClass().getSimpleName());
     }
 }
--- JavaProgrammingReviews_EU10/src/week15_12_17_2022/method_overriding/task1/GarantiBank.java
+++ JavaProgrammingReviews_EU10/src/week15_12_17_2022/method_overriding/task1/GarantiBank.java
@@ -6,5 +6,6 @@ public class GarantiBank extends Bank {
     public void calculateTheInterest() {
         super.calculateTheInterest();
         System.out.println(getCapital() * 8 / 100);
+
     }
 }
