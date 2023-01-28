c16153a9d6915f01a703497b2b54ac286642fc10
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-28 19:55:43 +0300

    week21_01_28_2023.map

---
+++ JavaProgrammingReviews_EU10/src/week21_01_28_2023/map/Test1.java
@@ -0,0 +1,32 @@
+package week21_01_28_2023.map;
+
+import week21_01_28_2023.map.Spartan;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Map<String ,String > spartan1 = new HashMap<>();
+        // add key and value
+        spartan1.put("id","1");
+        spartan1.put("name","Adam");
+        spartan1.put("gender","M");
+        System.out.println(spartan1);
+
+
+        Map<String ,String > spartan2 = new HashMap<>();
+        Spartan spartan = new Spartan();
+        spartan2.put("id","2");
+        spartan.setId("2");
+
+        spartan2.put("name","Burhan");
+        spartan.setName("Burhan");
+
+        spartan2.put("gender","M");
+        spartan.setGender("M");
+
+        System.out.println(spartan2.get("id"));
+        System.out.println(spartan.getId());
+    }
+}
